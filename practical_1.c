4a379f3d794ae41c1b9748c39a59fe07529774f9
Ankush <ankushnathsharma50@gmail.com>
2025-12-31 12:59:40 +0530

    commited hb

---
+++ practical_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("ankush nath sharma\n");
+    printf("ERP :- 11809\n");
+
+    printf("Name: Ankush Nath Sharma\n");
+    printf("Age: 18\n");
+    return 0;
+}
